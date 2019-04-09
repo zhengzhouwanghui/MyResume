a99c17a219936f89cc9ab3da35882087e191ede5
wang <232251598@qq.com>
2019-04-09 19:02:19 +0800

    all

---
+++ myblog/precache-manifest.9974f9da2f6c5167d25cf34135aee5ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a4c82eb628040008667e",
+    "url": "/js/chunk-vendors.8cf29967.js"
+  },
+  {
+    "revision": "fa0ae45300c3e1d86fb6",
+    "url": "/js/app.6a66b246.js"
+  },
+  {
+    "revision": "0745357e5bca68f5d558e5230eaa8cf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "a4c82eb628040008667e",
+    "url": "/css/chunk-vendors.ab8b7d8d.css"
+  },
+  {
+    "revision": "fa0ae45300c3e1d86fb6",
+    "url": "/css/app.0673541a.css"
+  }
+];
---
+++ usersys/precache-manifest.72509009b117ac3f6a8844237822fada.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7c85b89292d361478852",
+    "url": "/js/chunk-vendors.bb0a9dd3.js"
+  },
+  {
+    "revision": "db96da5d569abed2588c",
+    "url": "/js/app.647ecff1.js"
+  },
+  {
+    "revision": "df7f4f2ab88b24fc8e35b9e4269731de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db96da5d569abed2588c",
+    "url": "/css/app.60296722.css"
+  }
+];
